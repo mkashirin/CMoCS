2678662420a613cade5152061c3c3578ea8a0b49
Michael Kashirin <88917554+mkashirin@users.noreply.github.com>
2023-11-17 23:51:29 +0300

    Add files via upload

--- CMoCSm/SoLE/solvers.py
+++ CMoCSm/SoLE/solvers.py
@@ -15,18 +15,18 @@ from .exceptions import LinearAlgebraError
 class Solver(ABC):
     def __init__(self):
         self.coefficient_matrix = None
-        self.scalars = None
+        self.scalars_vector = None
         self.augmented_matrix = None
-        self.solution_matrix = None
+        self.solution_vector = None
 
     @staticmethod
     def _apply_partial_pivoting(matrix: ndarray, iteration: int, rows: int):
         """
-        Performs partial pivoting to avoid division by zero.
+        Perform partial pivoting to avoid division by zero.
 
             :param matrix: Augmented matrix to which partial pivoting would be applied;
             :param iteration: current iteration over rows of the given matrix;
-            :param rows: number of rows of the given matrix.
+            :param rows: number of rows in the given matrix.
             :return: Given matrix with partial pivoting applied to it.
         """
 
@@ -40,13 +40,12 @@ class Solver(ABC):
             )
         return matrix
 
-    def fit(self, coefficient_matrix: ndarray, scalars: ndarray):
+    def fit(self, coefficient_matrix: ndarray, scalars_vector: ndarray):
         """
-        Checks if given coefficient_matrix is consistent and independent and
-        scalars matrix has only one column.
+        Check if given coefficient_matrix is consistent, independent and scalars vector has only one column.
 
             :param coefficient_matrix: Matrix of coefficients;
-            :param scalars: column matrix of scalars.
+            :param scalars_vector: column vector of scalars vector.
         """
 
         if coefficient_matrix.shape[0] != coefficient_matrix.shape[1]:
@@ -54,14 +53,14 @@ class Solver(ABC):
                 "Matrix A should be square"
             )
 
-        if scalars.shape[1] > 1 or scalars.shape[0] != scalars.shape[0]:
+        if scalars_vector.shape[1] > 1 or scalars_vector.shape[0] != scalars_vector.shape[0]:
             raise LinearAlgebraError(
                 "Matrix B should be a column matrix with the same number of rows as matrix A"
             )
 
-        self.coefficient_matrix, self.scalars = coefficient_matrix, scalars
+        self.coefficient_matrix, self.scalars_vector = coefficient_matrix, scalars_vector
         self.augmented_matrix = concatenate(
-            (self.coefficient_matrix, self.scalars), axis=1, dtype=float64
+            (self.coefficient_matrix, self.scalars_vector), axis=1, dtype=float64
         )
 
     @abstractmethod
@@ -69,8 +68,8 @@ class Solver(ABC):
         pass
 
 
-class GaussianEliminator(Solver):
-    """Solve systems of linear equations using Gaussian Elimination method with partial pivoting."""
+class GaussianEliminationSolver(Solver):
+    """Solve independent systems of linear equations using Gaussian Elimination method with partial pivoting."""
 
     def __init__(self):
         super().__init__()
@@ -78,11 +77,11 @@ class GaussianEliminator(Solver):
     @staticmethod
     def _eliminate(matrix: ndarray, iteration: int, rows: int):
         """
-        Performs Gaussian Elimination algorythm.
+        Perform Gaussian Elimination algorythm.
 
             :param matrix: Augmented matrix to which Gaussian Elimination would be applied;
             :param iteration: current iteration over rows of the given matrix;
-            :param rows: number of rows of the given matrix.
+            :param rows: number of rows in the given matrix.
             :return: Given matrix with Gaussian Elimination applied to it.
         """
 
@@ -92,41 +91,41 @@ class GaussianEliminator(Solver):
         return matrix
 
     @staticmethod
-    def _substitute_backwards(solutions: ndarray, matrix: ndarray, rows: int):
+    def _substitute_backwards(scalars_vector: ndarray, matrix: ndarray, rows: int):
         """
-        Performs backward substitution to compute solution matrix.
+        Substitute backwards to compute solution matrix.
 
-            :param solutions: Input solution matrix with the last element being resolved;
+            :param scalars_vector: Input solution matrix with the last element being resolved;
             :param matrix: augmented matrix in the triangular form;
             :param rows: number of rows in the augmented matrix.
             :return: Solution matrix.
         """
 
         for row in arange(rows - 2, -1, -1):
-            solutions[row] = matrix[row][rows]
+            scalars_vector[row] = matrix[row][rows]
 
             for column in arange(row + 1, rows):
-                solutions[row] = solutions[row] - matrix[row][column] * solutions[column]
-            solutions[row] = solutions[row] / matrix[row][row]
+                scalars_vector[row] = scalars_vector[row] - matrix[row][column] * scalars_vector[column]
+            scalars_vector[row] = scalars_vector[row] / matrix[row][row]
 
-        return solutions
+        return scalars_vector
 
-    def fit(self, coefficient_matrix: ndarray, scalars: ndarray):
-        super().fit(coefficient_matrix, scalars)
+    def fit(self, coefficient_matrix: ndarray, scalars_vector: ndarray):
+        super().fit(coefficient_matrix, scalars_vector)
 
     def solve(self, round_to: int):
         """
-        Solves the system of linear equations passed to the fit() method.
+        Solve the independent system of linear equations.
 
             :param round_to: Number of decimals to which solution should be rounded.
             :return: Solution matrix for the given system of linear equations.
         """
 
         augmented_matrix = self.augmented_matrix
-        rows = len(self.scalars)
-        last = rows - 1
+        rows = len(self.scalars_vector)
+        index = rows - 1
         iteration = 0
-        solution_matrix = zeros(rows)
+        solution_vector = zeros(rows)
 
         while iteration < rows:
             augmented_matrix = self._apply_partial_pivoting(
@@ -138,152 +137,149 @@ class GaussianEliminator(Solver):
             )
             iteration = iteration + 1
 
-        solution_matrix[last] = augmented_matrix[last][rows] / augmented_matrix[last][last]
+        solution_vector[index] = augmented_matrix[index][rows] / augmented_matrix[index][index]
 
-        solution_matrix = self._substitute_backwards(
-            solution_matrix, augmented_matrix, rows=rows
+        solution_vector = self._substitute_backwards(
+            solution_vector, augmented_matrix, rows=rows
         )
-        solution_matrix = solution_matrix.round(round_to)
-        self.solution_matrix = solution_matrix.reshape(rows, 1)
+        self.solution_vector = solution_vector.round(round_to).reshape(rows, 1)
 
-        return self.solution_matrix
+        return self.solution_vector
 
 
-class SquaresMinimizer(Solver):
-    """Solve systems of linear equations using Least Squares method."""
+class LeastSquaresSolver(Solver):
+    """LeastSquaresSolver solves dependent systems of linear equations using Least Squares method."""
 
     def __init__(self):
         super().__init__()
-        self.gaussian_eliminator = GaussianEliminator()
+        self.gaussian_eliminator = GaussianEliminationSolver()
 
-    def fit(self, coefficient_matrix: ndarray, scalars: ndarray):
+    def fit(self, coefficient_matrix: ndarray, scalars_vector: ndarray):
         """
-        Performs some necessary computations to transform given matrices in appropriate for GaussianEliminator form.
+        Perform computations to transform given matrices in appropriate for GaussianEliminationSolver form.
 
             :param coefficient_matrix: Matrix of coefficients;
-            :param scalars: column matrix of scalars.
+            :param scalars_vector: Column vector of scalars.
         """
         transposed = coefficient_matrix.T
         self.coefficient_matrix = transposed @ coefficient_matrix
-        self.scalars = transposed @ scalars
-        self.gaussian_eliminator.fit(self.coefficient_matrix, self.scalars)
+        self.scalars_vector = transposed @ scalars_vector
+        self.gaussian_eliminator.fit(self.coefficient_matrix, self.scalars_vector)
 
     def solve(self, round_to: int):
         """
-        Solves the transformed system of linear equations using Gaussian Elimination method.
+        Solve the transformed system of linear equations using Gaussian Elimination method.
 
             :param round_to: Number of decimals to which solution should be rounded.
-            :return: Solution matrix for the given system of linear equations.
+            :return: Solution vector for the given system of linear equations.
         """
         return self.gaussian_eliminator.solve(round_to)
 
 
-class TridiagonalSolver(Solver):
-    """Attempt to implement Thomas algorythm for tridiagonal systems of linear equation."""
+class TridiagonalMatrixSolver(Solver):
+    """TridiagonalSolver solves tridiagonal systems of linear equations using Tridiagonal Matrix Method."""
 
     def __init__(self):
         super().__init__()
-        self.run_trough_matrix_p = None
-        self.run_trough_matrix_q = None
 
     @staticmethod
-    def _assign_diagonals(coefficient_matrix: ndarray, last: int):
+    def _get_diagonals(coefficient_matrix: ndarray, index: int):
         """
-        Parses the given coefficient matrix to get diagonal vectors.
+        Parse the given coefficient matrix to extract diagonals properly.
 
             :param coefficient_matrix: Matrix of coefficients;
-            :param last: index of the last row of the coefficient matrix.
+            :param index: index of the last row in the coefficient matrix.
             :return: Tuple of diagonals parsed.
         """
         alpha, beta, gamma = (
-            insert(coefficient_matrix.diagonal(-1), obj=0, values=.0),
-            coefficient_matrix.diagonal(0),
-            insert(coefficient_matrix.diagonal(1), obj=last, values=0.0)
+                insert(coefficient_matrix.diagonal(-1), obj=0, values=0.0),
+                coefficient_matrix.diagonal(0),
+                insert(coefficient_matrix.diagonal(1), obj=index, values=0.0)
         )
         return alpha, beta, gamma
 
     @staticmethod
-    def _apply_recurrent_formula(
-            alpha: ndarray, beta: ndarray, gamma: ndarray,
-            coefficient_e: ndarray, coefficient_f: ndarray, last: int
+    def _run_forward(
+            scalars_vector: ndarray, rows: int, alpha: ndarray, beta: ndarray, gamma: ndarray,
+            coefficient_p: ndarray, coefficient_q: ndarray
     ):
         """
-        Applies recurrent formula to get corresponding values of e and f for the computational part.
+        Perform forward run-trough.
 
-            :param alpha: Coefficient alpha;
-            :param beta: coefficient beta;
-            :param gamma: coefficient gamma;
-            :param coefficient_e: solving coefficient e;
-            :param coefficient_f: solving coefficient f;
-            :param last: index of the last row of the coefficient matrix.
-            :return: Solving coefficient matrices e and f.
+            :param scalars_vector: Column vector of scalars_vector;
+            :param rows: number of rows of the matrix;
+            :param alpha: alpha;
+            :param beta: beta;
+            :param gamma: gamma;
+            :param coefficient_p: P;
+            :param coefficient_q: Q.
+            :return: Run-trough coefficients matrices P and Q.
         """
-        for row in arange(1, last):
-            denominator = beta[row] - alpha[row] * coefficient_e[row - 1]
-            coefficient_e[row] = gamma[row] / denominator
-            coefficient_f[row] = (
-                (beta[row] - alpha[row] * coefficient_f[row - 1]) /
-                denominator
+        for row in arange(1, rows):
+            denominator = beta[row] - alpha[row] * coefficient_p[row - 1]
+            coefficient_p[row] = gamma[row] / denominator
+            coefficient_q[row] = (
+                    (scalars_vector[row] - alpha[row] * coefficient_q[row - 1]) /
+                    denominator
             )
-        return coefficient_e, coefficient_f
+        return coefficient_p, coefficient_q
 
     @staticmethod
-    def _compute_backwards(
-            solution_matrix: ndarray, rows: int, alpha: ndarray, beta: ndarray,
-            coefficient_e: ndarray, coefficient_f: ndarray
+    def _run_backwards(
+            solution_vector: ndarray, rows: int, coefficient_p: ndarray,
+            coefficient_q: ndarray
     ):
         """
-        Computes the solution matrix moving upwards.
+        Perform backward run-trough.
 
-            :param solution_matrix: Solution matrix in which the first row is computed;
-            :param rows: number of rows;
-            :param alpha: coefficient alpha;
-            :param beta: coefficient beta;
-            :param coefficient_e: solving coefficient e;
-            :param coefficient_f: solving coefficient f;
-            :return: The solution matrix.
+            :param solution_vector: Solution matrix in which the first row is computed;
+            :param coefficient_p: P;
+            :param coefficient_q: Q.
+            :return: The solution vector.
         """
-        solution_matrix[rows - 1] = (
-                (alpha[rows - 1] * coefficient_f[rows - 2] - beta[rows - 1]) /
-                (alpha[rows - 1] * coefficient_e[rows - 2] - beta[rows - 1])
-        )
+        solution_vector[rows - 1] = coefficient_q[rows - 1]
         for row in arange(rows - 2, -1, -1):
-            solution_matrix[row] = (
-                    coefficient_f[row] - coefficient_e[row] * solution_matrix[row + 1]
+            solution_vector[row] = (
+                    coefficient_q[row] - coefficient_p[row] * solution_vector[row + 1]
             )
-        return solution_matrix
+        return solution_vector
 
-    def fit(self, coefficient_matrix: ndarray, scalars: ndarray):
-        super().fit(coefficient_matrix, scalars)
+    def fit(self, coefficient_matrix: ndarray, scalars_vector: ndarray):
+        super().fit(coefficient_matrix, scalars_vector)
 
     def solve(self, round_to: int):
-        scalars = self.scalars
-        rows = len(self.scalars)
-        last = rows - 1
+        """
+        Solve the independent system of linear equations.
+
+            :param round_to: Number of decimals to which solution should be rounded.
+            :return: Solution vector for the given system of linear equations.
+        """
+        scalars_vector = self.scalars_vector
+        rows = len(self.scalars_vector)
+        index = rows - 1
         coefficient_matrix = self.coefficient_matrix
-        coefficient_alpha, coefficient_beta, coefficient_gamma = (
-            self._assign_diagonals(coefficient_matrix, last=last)
+        alpha, beta, gamma = (
+            self._get_diagonals(coefficient_matrix, index=index)
         )
-        coefficient_e, coefficient_f = zeros(rows), zeros(rows)
-        solution_matrix = zeros(rows)
+        coefficient_p, coefficient_q = zeros(rows), zeros(rows)
+        solution_vector = zeros(rows)
 
-        coefficient_e[0] = coefficient_gamma[0] / coefficient_beta[0]
-        coefficient_f[0] = scalars[0] / coefficient_beta[0]
+        coefficient_p[0] = gamma[0] / beta[0]
+        coefficient_q[0] = scalars_vector[0] / beta[0]
 
-        coefficient_e, coefficient_f = (
-            self._apply_recurrent_formula(
-                alpha=coefficient_alpha, beta=coefficient_beta, gamma=coefficient_gamma,
-                coefficient_e=coefficient_e, coefficient_f=coefficient_f, last=last
+        coefficient_p, coefficient_q = (
+            self._run_forward(
+                scalars_vector,  rows=rows, alpha=alpha, beta=beta, gamma=gamma,
+                coefficient_p=coefficient_p, coefficient_q=coefficient_q
             )
         )
 
-        solution_matrix = (
-            self._compute_backwards(
-                solution_matrix, rows=rows, alpha=coefficient_alpha, beta=coefficient_beta,
-                coefficient_e=coefficient_e, coefficient_f=coefficient_f
+        solution_vector = (
+            self._run_backwards(
+                solution_vector, rows=rows, coefficient_p=coefficient_p,
+                coefficient_q=coefficient_q
             )
         )
-        solution_matrix = solution_matrix.round(round_to).reshape(rows, 1)
-        self.solution_matrix = solution_matrix
+        self.solution_vector = solution_vector.round(round_to).reshape(rows, 1)
 
-        return self.solution_matrix
+        return self.solution_vector
